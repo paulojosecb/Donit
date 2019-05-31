8001dedc3031ca3900f3991e0a5098b5f94985f0
Paulo José <paulojocb@gmail.com>
2019-05-31 09:16:07 -0300

    Create workspace and Home Module

---
+++ Features/Home/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Home
+//
+//  Created by Paulo José on 31/05/19.
+//  Copyright © 2019 Paulo José. All rights reserved.
+//
+
+import UIKit
+
+public class HomeViewController: UIViewController {
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
