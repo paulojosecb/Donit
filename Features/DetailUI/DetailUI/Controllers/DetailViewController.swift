94e069508fdb6d2e6459f8e541fd5b3ae707359a
Paulo José <paulojocb@gmail.com>
2019-05-31 09:35:26 -0300

    Add DetailViewController on DetailUI

---
+++ Features/DetailUI/DetailUI/Controllers/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  DetailUI
+//
+//  Created by Paulo José on 31/05/19.
+//  Copyright © 2019 Paulo José. All rights reserved.
+//
+
+import UIKit
+import HomeUI
+
+public class DetailViewController: UIViewController {
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let vc = HomeViewController()
+        present(vc, animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
