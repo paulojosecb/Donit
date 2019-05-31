bbd3f0114761a643f914f0ec6320cbb09762d596
Paulo José <paulojocb@gmail.com>
2019-05-31 09:26:09 -0300

    Add Features Models

---
+++ Features/DetailUI/DetailUI/DetailUI.h
@@ -0,0 +1,19 @@
+//
+//  DetailUI.h
+//  DetailUI
+//
+//  Created by Paulo José on 31/05/19.
+//  Copyright © 2019 Paulo José. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DetailUI.
+FOUNDATION_EXPORT double DetailUIVersionNumber;
+
+//! Project version string for DetailUI.
+FOUNDATION_EXPORT const unsigned char DetailUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DetailUI/PublicHeader.h>
+
+
