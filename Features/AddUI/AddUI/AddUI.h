f7a3ee25103f4eb73b7562686b849f075906e783
Paulo José <paulojocb@gmail.com>
2019-05-31 09:38:42 -0300

    Create AddUI Module

---
+++ Features/AddUI/AddUI/AddUI.h
@@ -0,0 +1,19 @@
+//
+//  AddUI.h
+//  AddUI
+//
+//  Created by Paulo José on 31/05/19.
+//  Copyright © 2019 Paulo José. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AddUI.
+FOUNDATION_EXPORT double AddUIVersionNumber;
+
+//! Project version string for AddUI.
+FOUNDATION_EXPORT const unsigned char AddUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AddUI/PublicHeader.h>
+
+
---
+++ Features/AddUI/AddUITests/AddUITests.swift
@@ -0,0 +1,34 @@
+//
+//  AddUITests.swift
+//  AddUITests
+//
+//  Created by Paulo José on 31/05/19.
+//  Copyright © 2019 Paulo José. All rights reserved.
+//
+
+import XCTest
+@testable import AddUI
+
+class AddUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
