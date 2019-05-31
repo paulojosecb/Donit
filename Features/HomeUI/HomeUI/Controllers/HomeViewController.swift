a875eaa79df59958120549c253a31d1857c53e70
Paulo José <paulojocb@gmail.com>
2019-05-31 09:29:19 -0300

    Create HomeViewController on HomeUI

---
+++ Features/HomeUI/HomeUI/Controllers/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  HomeUI
+//
+//  Created by Paulo José on 31/05/19.
+//  Copyright © 2019 Paulo José. All rights reserved.
+//
+
+import UIKit
+
+public class HomeViewController: UIViewController {
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
