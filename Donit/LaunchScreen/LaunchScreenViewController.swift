84e266acf742bf2e04e61cb34347f614b8a89ae9
Paulo José <paulojocb@gmail.com>
2019-06-08 15:53:09 -0300

    Move launcshcreen

---
+++ Donit/LaunchScreen/LaunchScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LaunchScreenViewController.swift
+//  Donit
+//
+//  Created by Paulo José on 14/12/18.
+//  Copyright © 2018 Paulo José. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
